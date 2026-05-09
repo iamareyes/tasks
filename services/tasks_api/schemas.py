06a722d2b422036ff6f015386f39a3b626c95981
Adolfo Reyes <adolfo0620@gmail.com>
2026-05-08 21:37:29 -0400

    adding getting list and closing task

---
+++ services/tasks_api/schemas.py
@@ -0,0 +1,31 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from models import TaskStatus, Task
+
+
+class CreateTask(BaseModel):
+    title: str
+
+
+class CloseTask(BaseModel):
+    id: UUID
+
+
+class APITask(BaseModel):
+    id: UUID
+    title: str
+    status: TaskStatus
+    owner: str
+
+    class Config:
+        orm_mode = True
+        
+        
+class APITaskList(BaseModel):
+    results: list[Task]
+
+    class Config:
+        orm_mode = True
+
